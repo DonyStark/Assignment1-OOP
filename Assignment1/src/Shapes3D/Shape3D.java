53fa9d58ee9b254df0d42675024fd13eebeccb24
nicoatsait <119918552+nicoatsait@users.noreply.github.com>
2023-10-29 17:12:20 -0600

    java doc

--- Assignment1/src/Shapes3D/Shape3D.java
+++ Assignment1/src/Shapes3D/Shape3D.java
@@ -7,20 +7,47 @@ package Shapes3D;
 	    
 	    protected double height;
 
-	    // Constructor
+	    /**
+	     * Constructs a `Shape3D` object with the given height.
+	     *
+	     * @param height The height of the three-dimensional shape.
+	     */
 	    public Shape3D(double height) {
 	        this.height = height;
 	    }
 
-	    // Abstract methods to be implemented by each shape
+	    /**
+	     * Abstract method to be implemented by each shape to calculate and return its volume.
+	     *
+	     * @return The volume of the three-dimensional shape.
+	     */
 	    public abstract double getVolume();
+	    
+	    /**
+	     * Abstract method to be implemented by each shape to calculate and return its base area.
+	     *
+	     * @return The base area of the three-dimensional shape.
+	     */
 	    public abstract double getBaseArea();
 
+	    
+	    /**
+	     * Compares this `Shape3D` object to another `Shape3D` object based on their heights.
+	     *
+	     * @param other The other `Shape3D` object to compare to.
+	     * @return A negative value if this shape's height is less than the other shape's height,
+	     *         a positive value if greater, or 0 if they have the same height.
+	     */
 	    @Override
 	    public int compareTo(Shape3D other) {
 	        return Double.compare(this.height, other.height);
 	    }
-
+	    
+	    
+	    /**
+	     * Retrieves the height of the three-dimensional shape.
+	     *
+	     * @return The height of the three-dimensional shape.
+	     */
 		protected abstract double getHeight();
 	}
-
